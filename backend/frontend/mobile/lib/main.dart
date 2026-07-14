3535caa871e66a6790bdaac54e8ecb1d2df0802a
Jesús Alejandro Montoya Cepeda <jesus.montoya3083@utc.edu.ec>
2026-07-14 13:50:58 -0500

    flutter o muestra en celular

---
+++ backend/frontend/mobile/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hola Mundo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Mi Primera App Flutter'),
+          backgroundColor: Colors.blue,
+        ),
+        body: const Center(
+          child: Text(
+            '¡Hola Mundo!',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
